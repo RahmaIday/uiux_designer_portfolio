d0cefdccd8e899321a49b7e09fa2d954912732f8
RahmaIday <rahma.idayath@outlook.com>
2025-08-31 02:01:03 +1000

    add nav link change logo

--- src/sections/Navbar.js
+++ src/sections/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 import Logo from '../assets/images/logo.png';
 import Details from '../details/Intro.js';
@@ -7,11 +8,15 @@ function NavBar() {
 
     return (
         <div className="navbar-container">
+            <Link to="/uiux_designer_portfolio/">
             <img src={Logo} className='logo-image' alt='Nav-logo' />
+            </Link>
+
             <div>
                 <div className='name'>{Details.Name.toUpperCase()}</div>
                 <div className='position'>{Details.Position.toUpperCase()}</div>
             </div>
+
         </div>
     );
 }
